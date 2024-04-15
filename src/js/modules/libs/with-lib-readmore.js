83af5a6b877acc01d2708f93e2d85531f42b1714
bokovushka <andriibokovyi@gmail.com>
2024-04-15 16:50:14 +0300

    update files

--- src/js/modules/libs/with-lib-readmore.js
+++ src/js/modules/libs/with-lib-readmore.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import '../../../../node_modules/readmore-js/readmore.js';
 
-$('.spoiler--seo').readmore({
+$('.spoiler').readmore({
 	speed: 75,
 	collapsedHeight: false,
 	moreLink: '<button class="btn-read-more btn-default btn btn-block btn-outline-main-blue text-uppercase mt-5 col col-md-6 col-lg-4 mx-auto">Показати</button>',

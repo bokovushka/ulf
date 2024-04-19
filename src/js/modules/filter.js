a740e5a884fb0f6539454e6bebe4101d4251c2b2
bokovushka <andriibokovyi@gmail.com>
2024-04-19 19:29:57 +0300

    update files

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -1,48 +1,29 @@
 import $ from 'jquery';
 
 //? mobile menu lock body
-$(document).ready(function () {
-	let bodyFilterLock = document.querySelector('body');
-	let btnFilter = document.querySelector('.btn-filter');
-	let bgBodyFilter = document.querySelector('.popup-bg-body-filter');
-	let isMenuFilterEnabled = true;
+let bodyFilterLock = document.querySelector('body');
+let btnFilter = document.querySelector('.btn-filter');
+let bgBodyFilter = document.querySelector('.popup-bg-body-filter');
+let isMenuFilterEnabled = true;
 
+if (btnFilter) {
 	btnFilter.addEventListener('click', function (event) {
 		if (!isMenuFilterEnabled) {
 			event.preventDefault();
 		} else {
 			bodyFilterLock.classList.add('lock-filter');
-			bgBodyFilter.classList.add('open')
+			bgBodyFilter.classList.add('open');
 			isMenuFilterEnabled = false;
 			setTimeout(function () {
 				isMenuFilterEnabled = true;
 			}, 1000);
 		}
 	});
+}
 
-	setInterval(function () {
-		if ($(bgBodyFilter).hasClass('collapsed')) {
-			bodyFilterLock.classList.remove('lock-filter');
-			bgBodyFilter.classList.remove('open')
-		}
-	}, 100);
-});
-
-//?  закриття меню поза областю
-// $(document).mouseup(function (e) {
-// 	if (!$(".sidebar").is(e.target) // если клик был не по нашему блоку
-// 		&& $(".sidebar").has(e.target).length === 0) { // и не по его дочерним элементам
-// 		$('.popup-bg-body-filter').removeClass("open");
-// 		$('body').removeClass("lock-filter");
-// 	}
-// });
-
-// //? закриття popup-bg-body і мобільного меню при поворотах наприклад на планшетці
-// setInterval(function () {
-// 	if (window.innerWidth >= 1024) {
-// 		if ($(".btn-filter").hasClass("show")) {
-// 			$('.btn-filter').click()
-// 		}
-// 	}
-// }, 1000);
-
+setInterval(function () {
+	if (bgBodyFilter && $(bgBodyFilter).hasClass('collapsed')) {
+		bodyFilterLock.classList.remove('lock-filter');
+		bgBodyFilter.classList.remove('open');
+	}
+}, 100);
--- src/js/modules/libs/with-lib-gsap.js
+++ src/js/modules/libs/with-lib-gsap.js
@@ -3,22 +3,28 @@ import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
-//left to right
-let horizontalSection = document.querySelector('.horizontal');
+document.addEventListener('DOMContentLoaded', function () {
+	let horizontalSection = document.querySelector('.horizontal');
 
-console.log(horizontalSection.scrollWidth);
+	if (horizontalSection) { // перевіряємо, чи існує елемент
+		console.log(horizontalSection.scrollWidth);
 
-gsap.to('.horizontal', {
-	x: () => horizontalSection.scrollWidth * -1,
-	xPercent: 100,
-	scrollTrigger: {
-		// markers: true,
-		scrub: true,
-		trigger: '.horizontal',
-		start: () => "center 50%",
-		end: () => "+=" + document.querySelector(".horizontal").offsetWidth,
-		pin: '.scroll-trigger .container-wrap',
-		scrub: true,
-		invalidateOnRefresh: true
+		gsap.to('.horizontal', {
+			x: () => horizontalSection.scrollWidth * -1,
+			xPercent: 100,
+			scrollTrigger: {
+				// markers: true,
+				scrub: true,
+				trigger: '.horizontal',
+				start: () => "center 50%",
+				end: () => "+=" + horizontalSection.offsetWidth, // використовуємо offsetWidth без додаткового запиту до DOM
+				pin: '.scroll-trigger .container-wrap',
+				scrub: true,
+				invalidateOnRefresh: true
+			}
+		});
 	}
+	// else {
+	// 	console.error("Element with class 'horizontal' not found on the page.");
+	// }
 });
